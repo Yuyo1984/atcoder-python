2bdfecf72b637594700d077d57092c148245c4e1
Yuyo1984 <yuyoyanase0909@gmail.com>
2024-05-17 11:28:52 +0900

    abc256-bのコードを反映

--- abc/abc256/b/main.py
+++ abc/abc256/b/main.py
@@ -1,4 +1,16 @@
-from sys import stdin
-input = stdin.readline
+n = int(input())
+A = list(map(int, input().split()))
+b = [0] * 4
+p = 0
+for i in range(n):
+    b[0] += 1
+    x = A[i]
+    for j in range(3, -1, -1):
+        if j + x >= 4:
+            p += b[j]
+            b[j] = 0
+        else:
+            b[j+x] += b[j]
+            b[j] = 0
 
-
+print(p)
