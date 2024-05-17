701349b226a0214a32f81d0212f27fcf047c2344
Yuyo1984 <yuyoyanase0909@gmail.com>
2024-05-17 18:20:43 +0900

    今日解いたB問題のコードを反映

--- abc/abc243/b/main.py
+++ abc/abc243/b/main.py
@@ -1,4 +1,18 @@
-from sys import stdin
-input = stdin.readline
+n = int(input())
+a = [*map(int, input().split())]
+b = [*map(int, input().split())]
+
+hit = 0
+blow = 0
+
+for i in range(n):
+    if a[i] == b[i]:
+        hit += 1
+    else:
+        if a[i] in b:
+            blow += 1
+
+print(hit)
+print(blow)
 
 
