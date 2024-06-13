ef53c625267359fcb2404f5364732f12c7ff1a90
Yuyo1984 <yuyoyanase0909@gmail.com>
2024-06-13 11:48:58 +0900

    反映したくないディレクトリとファイルを追跡しないように設定

--- goriv/4.py
+++
@@ -1,6 +0,0 @@
-N = int(input())
-A = set()
-for i in range(N):
-    A.add((tuple(input().split())))
-
-print(len(A))
--- main.py
+++
@@ -1,7 +0,0 @@
-h, m = input().split(":")
-h = int(h)
-m = int(m)
-
-h += 8
-h %= 24
-print(str(h) + ":" + str(m))
