78ff412ec10b4fce30c5ae861c3e4c929c0fd39e
Yuyo1984 <yuyoyanase0909@gmail.com>
2024-07-09 14:23:53 +0900

    バチャ用のディレクトリ作成

--- abc/abc353/d/main.py
+++ abc/abc353/d/main.py
@@ -1,5 +1,14 @@
-#input
+from itertools import accumulate
 
-#solve
-
-#output
+# input
+N = int(input())
+A = [*map(int, input().split())]
+l = [len(str(x)) for x in A]
+# solve
+ans = 0
+for i in range(N):
+    s = A[i]
+    ans += s
+    ans %= 998244353
+# output
+print(ans)
