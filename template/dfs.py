7bfeacca128718cdc622bf39cd9d4d1b6a58b4bc
Yuyo1984 <yuyoyanase0909@gmail.com>
2024-07-20 02:55:00 +0900

    テンプレートを整備

--- template/dfs.py
+++ template/dfs.py
@@ -1,8 +1,10 @@
-# import sys
+import sys
 
-# sys.setrecursionlimit(10**6)
+sys.setrecursionlimit(10**6)
 
 
+# Depth First Search
+#
 def dfs(graph, node, visited):
     visited.add(node)
     for neighbor in graph[node]:
@@ -10,22 +12,9 @@ def dfs(graph, node, visited):
             dfs(graph, neighbor, visited)
 
 
-def is_connected(graph, n):
-    start_node = 1
-    visited = set()
-    dfs(graph, start_node, visited)
-
-    return len(visited) == n
-
-
-n, m = map(int, input().split())
-graph = {i: [] for i in range(1, n + 1)}
-for i in range(m):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
-
-if is_connected(graph, n):
-    print("The graph is connected.")
-else:
-    print("The graph is not connected.")
+# n, m = map(int, input().split())
+# graph = {i: [] for i in range(1, n + 1)}
+# for i in range(m):
+# a, b = map(int, input().split())
+# graph[a].append(b)
+# graph[b].append(a)
